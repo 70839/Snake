87d50e0a71f4ec65e883bfa4f87e6e5e06e9065c
Hero_Ahren <ahrenmurray07@gmail.com>
2022-11-29 16:59:16 -0500

    Update index.js

--- Src/Js/index.js
+++ Src/Js/index.js
@@ -1,1 +1,30 @@
-var x = 0;
+//Code from W3schools
+var myGamePiece;
+
+function startGame() {
+  myGameArea.start();
+  myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+var myGameArea = {
+  canvas : $("#game"),
+  start : function() {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+  }
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+
+
+//My code
